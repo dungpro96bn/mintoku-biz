885d6127129269da6c31e7a1842c5aa7426f274b
N1Tung <scvweb.n1tung@gmail.com>
2024-12-23 09:47:10 +0700

    tung-pageparent-12/23/24

--- wp-content/themes/GMS/serviceCloudMaetra.php
+++ wp-content/themes/GMS/serviceCloudMaetra.php
@@ -53,19 +53,20 @@
                         <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudmaetra_immigration.png" alt="">
                     </picture>
                 </div>
-<!--                CAU HOI THUONG GAP-->
-                <div class="normal-qa">
-                    <h2 class="heading-block en">
-                        <span class="uppercase">Frequently asked questions</span>
-                    </h2>
-                    <h3 class="title">よくあるお悩み</h3>
-                    <div class="">
-                        <picture class="image">
-                            <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudmaetra_asked.png">
-                            <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudmaetra_asked.png">
-                            <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudmaetra_asked.png" alt="">
-                        </picture>
-                    </div>
+
+            </div>
+            <!--                CAU HOI THUONG GAP-->
+            <div class="normal-qa">
+                <h2 class="heading-block en">
+                    <span class="uppercase">Frequently asked questions</span>
+                </h2>
+                <h3 class="title">よくあるお悩み</h3>
+                <div class="asked-question">
+                    <picture class="image">
+                        <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudmaetra_asked.png">
+                        <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudmaetra_asked.png">
+                        <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudmaetra_asked.png" alt="">
+                    </picture>
                 </div>
             </div>
 
@@ -77,8 +78,7 @@
                     <h2 class="heading-block">
                         <span class="uppercase">SOLUTION</span>
                     </h2>
-                    <p class="sub-ttl">現場作業の動画や入社前後の教育動画を<br>
-                        いつでもどこでも母国語で説明できるサービス</p>
+                    <p class="sub-ttl">前トレは、動画やVRを使ってそんなお悩みを解決いたします</p>
                 </div>
                 <div class="solution-content">
                     <div class="solution-list">
@@ -90,24 +90,24 @@
                             </div>
                             <div class="image-content">
                                 <picture class="image">
-                                    <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/solution-video-step01.png">
-                                    <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/solution-video-step01.png">
-                                    <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/solution-video-step01.png" alt="">
+                                    <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudvideostep_so01.png">
+                                    <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudvideostep_so01.png">
+                                    <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudvideostep_so01.png" alt="">
                                 </picture>
-                                <a class="play-button" href="https://app.videostep.io/manuals/dc736019-07dd-4e7b-95db-ebbe3f8f4735" target="_blank">
-                                    <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/playplay.png" alt="">
-                                </a>
                             </div>
                             <div class="info-content">
-                                <h4 class="title">現場での説明は<br>
-                                    トレーニング動画にお任せ</h4>
-                                <p class="t1">本来の自分の業務に集中できます。</p>
+                                <h4 class="title">前トレ with VideoStep</h4>
+                                <p class="t1">母国語の動画で、<br>
+                                    “伝える”から“伝わる”へ</p>
+                                <p class="text">
+                                    伝わらなかった業務は終わりです。何度も繰り返す説明を、母国語による動画で見せることで、より理解度とコミュニケーションの質を高めます。
+                                </p>
                                 <div class="link-page">
-                                    <a href="https://app.videostep.io/manuals/dc736019-07dd-4e7b-95db-ebbe3f8f4735" target="_blank">詳しく見る ＞</a>
+                                    <a href="#">詳しく見る ＞</a>
                                 </div>
                             </div>
                         </div>
-                        <!--                        2-->
+<!--                            2-->
                         <div class="solution-item">
                             <div class="box-number">
                                 <p class="t1">Feature</p>
@@ -115,149 +115,50 @@
                             </div>
                             <div class="image-content">
                                 <picture class="image">
-                                    <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/solution-video-step02.png">
-                                    <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/solution-video-step02.png">
-                                    <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/solution-video-step02.png" alt="">
+                                    <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudvideostep_so02.png">
+                                    <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudvideostep_so02.png">
+                                    <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudvideostep_so02.png" alt="">
                                 </picture>
-                                <a class="play-button" href="https://app.videostep.io/manuals/88724ac5-3823-40a6-b1b4-ba1ce1aa0ebb" target="_blank">
-                                    <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/playplay.png" alt="">
-                                </a>
                             </div>
                             <div class="info-content">
-                                <h4 class="title">会社の説明は<br>
-                                    パソコンで事前学習で</h4>
-                                <p class="t1">分からないことは何度でも確認。<br>
-                                    だから安心。</p>
+                                <h4 class="title">前トレ with edupoke</h4>
+                                <p class="t1">外国人材の教育プラットフォーム</p>
+                                <p class="text">
+                                    前トレwith日本語は日本で働く外国人材のための教育プラットフォームです。<br>
+                                    日本語教育に加え、業界初となる【Employability(雇用可能力、雇用されうる力）】を習得させるプログラムを提供し、本当に職場で活躍できる人材育成のお手伝いをします。
+                                </p>
                                 <div class="link-page">
-                                    <a href="https://app.videostep.io/manuals/88724ac5-3823-40a6-b1b4-ba1ce1aa0ebb" target="_blank">詳しく見る ＞</a>
+                                    <a href="#">詳しく見る ＞</a>
                                 </div>
                             </div>
                         </div>
-                        <!--                        3-->
-                        <div class="solution-item col-style">
+<!--                        3-->
+                        <div class="solution-item">
                             <div class="box-number">
                                 <p class="t1">Feature</p>
                                 <p class="t2">03</p>
                             </div>
-                            <div class="info-content">
-                                <h4 class="title">前トレ with VideoStepでできること</h4>
-                                <dl class="training-list">
-                                    <dt class="training-item fl">
-                                        <div class="box-text">
-                                            <p class="t1">多言語対応で<br>
-                                                世界各国の方も受け入れOK!</p>
-                                            <p class="text">英語/中国語(簡体)/中国語(繁体)/韓国語/ポルトガル語
-                                                ベトナム語/インドネシア語/タガログ語／モンゴル語/
-                                                スペイン語/マレー語/タイ語</p>
-                                        </div>
-                                        <div class="box-img">
-                                            <picture class="banner-logo">
-                                                <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/stepvideo-training01.png">
-                                                <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/stepvideo-training01.png">
-                                                <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/stepvideo-training01.png" alt="">
-                                            </picture>
-                                        </div>
-                                    </dt>
-
-                                    <dt class="training-item fl">
-                                        <div class="box-text">
-                                            <p class="t1">現場で動画が確認可能！<br>
-                                                場所を選びません。</p>
-                                            <p class="text">動画はQRコードで見れるので、現場で動画確認可能！
-                                                説明のための会議や、場所を用意する必要はありません。</p>
-                                        </div>
-                                        <div class="box-img">
-                                            <picture class="banner-logo">
-                                                <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/stepvideo-training02.png">
-                                                <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/stepvideo-training02.png">
-                                                <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/stepvideo-training02.png" alt="">
-                                            </picture>
-                                        </div>
-                                    </dt>
-
-                                    <dt class="training-item fl">
-                                        <div class="box-text">
-                                            <p class="t1">閲覧履歴で進捗確認も可能！<br>
-                                                何度でも繰り返し学べます。</p>
-                                            <p class="text">PC等で使用する場合は閲覧履歴も見れるのでそれぞれの進捗状況も確認できます！<br>
-                                                自宅でも閲覧できるので、就業前に学習してもらうことができます。</p>
-                                        </div>
-                                        <div class="box-img">
-                                            <picture class="banner-logo">
-                                                <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/stepvideo-training03.png">
-                                                <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/stepvideo-training03.png">
-                                                <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/stepvideo-training03.png" alt="">
-                                            </picture>
-                                        </div>
-                                    </dt>
-                                </dl>
-                            </div>
-                        </div>
-                        <!--                        4-->
-                        <div class="solution-item col-style">
-                            <div class="box-number">
-                                <p class="t1">Feature</p>
-                                <p class="t2">04</p>
+                            <div class="image-content">
+                                <picture class="image">
+                                    <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudvideostep_so03.png">
+                                    <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudvideostep_so03.png">
+                                    <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/servicecloudvideostep_so03.png" alt="">
+                                </picture>
                             </div>
                             <div class="info-content">
-                                <h4 class="title">前トレ with ビデオステップは<br>
-                                    よく使う教育動画コンテンツも実装しております。</h4>
-                                <dl class="edu-list fl">
-                                    <dt class="edu-item">
-                                        <div class="edu-item-inner">
-                                            <p class="t1">生活オリエンテーションパッケージ</p>
-                                            <p class="text">日常生活で困ることを45本の短編動画にまとめまし<br>た。</p>
-                                            <p class="label text">プラン内容</p>
-                                            <p class="text fl">
-                                                <span>・食事<br>
-                                                ・掃除・ゴミ出し<br>
-                                                ・家具家電<br>
-                                                ・生活ルール</span>
-                                                <span>
-                                                    ・交通ルール<br>
-                                                    ・お金<br>
-                                                    ・緊急トラブル
-                                                </span>
-                                            </p>
-
-                                        </div>
-                                        <div class="link-page">
-                                            <a href="#" >詳しく見る ＞</a>
-                                        </div>
-                                    </dt>
-
-                                    <dt class="edu-item">
-                                        <div class="edu-item-inner">
-                                            <p class="t1">入国パッケージ</p>
-                                            <p class="text">入管が推奨している「生活・就労ガイドブック」<br>
-                                                の内容をまとめて多言語化しました。</p>
-                                            <p class="label text">プラン内容</p>
-                                            <p class="text fl">
-                                                <span>第1章　入国・在留<br>
-                                                第2章　市町村での手続<br>
-                                                第3章　雇用・労働<br>
-                                                第4章　出産・子育て<br>
-                                                第5章　教育<br>
-                                                第６章　医療</span>
-                                                <span>
-                                                    第７章　年金・福祉<br>
-                                                    第８章　税金<br>
-                                                    第９章　交通<br>
-                                                    第１０章　緊急・<br>
-                                                    第１１章　住居<br>
-                                                    第１２章　日常におけるルール
-                                                    　　　　　習慣
-                                                </span>
-                                            </p>
-
-                                        </div>
-                                        <div class="link-page">
-                                            <a href="#" >詳しく見る ＞</a>
-                                        </div>
-                                    </dt>
-                                </dl>
+                                <h4 class="title">前トレ with VR</h4>
+                                <p class="t1">360°パノラマVRで事業者の業績UPに貢献</p>
+                                <p class="text">
+                                    空間データ活用プラットフォーム「前トレ with VR」は、<br>
+                                    360度VRコンテンツを、誰でも手軽に、簡単に制作、編集ができるクラウドソフトウェアです。
+                                </p>
+                                <div class="link-page">
+                                    <a href="#">詳しく見る ＞</a>
+                                </div>
                             </div>
                         </div>
+
+
                     </div>
                 </div>
 
