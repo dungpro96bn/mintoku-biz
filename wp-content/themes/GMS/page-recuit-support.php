42eb29aa182a6fdc1e2987ec91eb151f4eb42ba3
David Bui <dungpro96bn@gmail.com>
2025-07-09 10:52:25 +0700

    update support block

--- wp-content/themes/GMS/page-recuit-support.php
+++ wp-content/themes/GMS/page-recuit-support.php
@@ -63,8 +63,8 @@
                         </picture>
                     </div>
                     <div class="list-page">
-                        <a target="_blank" href="/biz/lp-seibishi/">ドライバー</a>
-                        <a target="_blank" href="/biz/lp-driver/">整備士</a>
+                        <a target="_blank" href="/biz/lp-driver/">ドライバー</a>
+                        <a target="_blank" href="/biz/lp-seibishi/">整備士</a>
                         <p class="coming-soon">Coming soon</p>
                         <p class="coming-soon">Coming soon</p>
                         <p class="coming-soon">Coming soon</p>
--- wp-content/themes/GMS/template-parts/support.php
+++ wp-content/themes/GMS/template-parts/support.php
@@ -17,7 +17,7 @@ if (is_page() || is_single()) {
 $slugs = ["translate", "camcat", "life-support", "maetra", "videostep", "edpoke", "vr"];
 ?>
 
-<div class="support <?php echo $slug; ?> <?php if($slug == "life-support" || $slug == "edpoke"){echo 'price-item02';} ?>" <?php if (in_array($slug, $slugs)) { echo 'style="display:none;"';} ?>>
+<div class="support <?php echo $slug; ?>" <?php if (in_array($slug, $slugs)) { echo 'style="display:none;"';} ?>>
     <div class="inner">
         <div class="reason-content">
             <div class="reason-info">
@@ -25,25 +25,78 @@ $slugs = ["translate", "camcat", "life-support", "maetra", "videostep", "edpoke"
                     <span>SUPPORT</span>
                 </h2>
                 <h3 class="title">導入後のサポート</h3>
-                <p class="text">スムーズにご利用いただくために、<br/>各種サポートを<br class="sp-br"/>ご用意しております</p>
+                <p class="text">スムーズにご利用いただくために、<br/>各種サポートをご用意しております</p>
                 <div class="link-page">
                     <a href="<?php echo home_url(); ?>/report_download/"><span>資料ダウンロード</span><span class="icon">＞</span></a>
                     <a href="<?php echo home_url(); ?>/contact/"><span>お問い合わせ</span><span class="icon">＞</span></a>
                 </div>
             </div>
             <div class="reason-image">
-                <?php if($slug == "life-support" || $slug == "edpoke" || $slug == "translate" || $slug == "consultation" || $slug == "camcat"): ?>
-                    <picture class="image">
-                        <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/support_image02_pc.png 2x">
-                        <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/support_image02_pc.png 2x">
-                        <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/support_image02_pc.png" alt="">
-                    </picture>
-                <?php else: ?>
-                    <picture class="image">
-                        <source srcset="<?php bloginfo('template_directory'); ?>/assets/images/top_image14_pc.png 2x">
-                        <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/top_image14_pc.png" alt="">
-                    </picture>
-                <?php endif; ?>
+                <div class="reasonInfo-list pc-br">
+                    <div class="reasonInfo-item">
+                        <div class="title">
+                            <picture class="image">
+                                <source srcset="<?php bloginfo('template_directory'); ?>/assets/images/support-icon01.png 2x">
+                                <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/support-icon01.png" alt="">
+                            </picture>
+                        </div>
+                        <div class="number-info">
+                            <p class="ttl">サポート満足度 97%<span>※2024年 自社調べ</span></p>
+                            <p class="text">お問合わせサポートやウェビナーなどをご用意。満足度97%のサポートで、<br/>経験豊富なスタップが迅速・丁寧に解決します。</p>
+                        </div>
+                    </div>
+                    <div class="reasonInfo-item">
+                        <div class="title">
+                            <picture class="image">
+                                <source srcset="<?php bloginfo('template_directory'); ?>/assets/images/support-icon02.png 2x">
+                                <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/support-icon02.png" alt="">
+                            </picture>
+                        </div>
+                        <div class="number-info">
+                            <p class="ttl">初心者の方でもご安心ください</p>
+                            <p class="text">システムを使い慣れていない方のために、<br/>豊富なマニュアルやサポートをご用意しています。</p>
+                        </div>
+                    </div>
+                    <div class="reasonInfo-item">
+                        <div class="title">
+                            <picture class="image">
+                                <source srcset="<?php bloginfo('template_directory'); ?>/assets/images/support-icon03.png 2x">
+                                <img class="sizes" src="<?php bloginfo('template_directory'); ?>/assets/images/support-icon03.png" alt="">
+                            </picture>
+                        </div>
+                        <div class="number-info">
+                            <p class="ttl">導入支援サービス（有償）</p>
+                            <p class="text">導入から運用開始までに必要な設定を専任の担当者がサポートします。前者展開前のテスト運用、車内周知から運用開始まで伴走することで、安心してご利用いただけます。</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="reasonInfo-list sp-br">
+                    <div class="reasonInfo-item">
+                        <div class="title">
+                            <p class="t1 icon">導入社数</p>
+                        </div>
+                        <div class="number-info">
+                            <p class="text"><span class="number">2,000</span>社</p>
+                            <p class="note sp-br">※2025年2月時点</p>
+                        </div>
+                    </div>
+                    <div class="reasonInfo-item">
+                        <div class="title">
+                            <p class="t1">事務対応時間</p>
+                        </div>
+                        <div class="number-info">
+                            <p class="text"><span class="number">55</span>%削減</p>
+                        </div>
+                    </div>
+                    <div class="reasonInfo-item">
+                        <div class="title">
+                            <p class="t1">対応可能言語数</p>
+                        </div>
+                        <div class="number-info">
+                            <p class="text"><span class="number">24</span>言語</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
