5c913edb71074991bd690a9f8c0bd4b96a39fa51
David Bui <dungpro96bn@gmail.com>
2025-07-18 09:42:24 +0700

    create mintoku-lp page

---
+++ wp-content/themes/GMS/page-mintoku-lp.php
@@ -0,0 +1,309 @@
+<!doctype html>
+<html lang="ja">
+	<head>
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-NXNV9MBM');</script>
+		<!-- End Google Tag Manager -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>外国人求人サイト - Mintoku Work｜“人手不足”に 世界から答えを。</title>
+		<meta name="description" content="外国人求人サイト - Mintoku Work｜“人手不足”に 世界から答えを。mintokuで始める、外国人採用のトータルサポート。">
+		<meta name="thumbnail" content="https://mintoku.com/work/images/header/banner_image.png">
+		<meta property="og:type" content="article">
+		<meta property="og:title" content="外国人求人サイト - Mintoku Work">
+		<meta property="og:site_name" content="外国人求人サイト - Mintoku Work">
+		<meta property="og:image" content="https://mintoku.com/work/images/header/banner_image.png">
+		<meta property="og:url" content="https://mintoku.com/work/">
+		<meta property="og:description" content="Mintoku Work (みんとくワーク)は、特定技能・技人国に特化した求人サイトです。">
+		<meta property="fb:app_id" content="387640167627398">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/reset.css">
+		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/style.css">
+		<link rel="icon" href="https://mintoku.com/work/favicon.ico">
+	</head>
+	<body>
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NXNV9MBM"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+		<header>
+			<div class="header">
+				<img src="<?php bloginfo('template_directory'); ?>/assets/img/mintoku-logo.svg" alt="mintoku work" class="logo">
+				<nav>
+					<ul>
+						<li class="pc-only">
+							<p><span>03-6738-9686</span><br>営業時間:10時-18時（月-金）</p>
+						</li>
+						<li>
+							<a href="https://mintoku.com/biz/seminar/">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/header-icon01.svg" alt="セミナー">
+							</a>
+						</li>
+						<li>
+							<a href="https://mintoku.com/biz/report_download/?category=white_paper">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/header-icon02.svg" alt="資料DL">
+							</a>
+						</li>
+						<li>
+							<a href="https://mintoku.com/biz/contact/">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/header-icon03.svg" alt="問い合わせ">
+							</a>
+						</li>
+						<li class="sp-only">
+							<a href="tel:03-6738-9686">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/header-icon04.svg" alt="電話">
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<main>
+			<section class="fv">
+				<picture class="kv">
+					<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/kv-sp.jpg" media="(max-width: 767px)">
+					<img src="<?php bloginfo('template_directory'); ?>/assets/img/kv-pc.jpg" alt="外国人イメージ">
+				</picture>
+				<div class="heading1">
+					<h1>
+						<picture>
+							<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/h1-sp.svg" media="(max-width: 767px)">
+							<img src="<?php bloginfo('template_directory'); ?>/assets/img/h1-pc.svg" alt="“人手不足”に 世界から答えを。mintokuで始める、外国人採用のトータルサポート。">
+						</picture>
+					</h1>
+				</div>
+			</section>
+			<section class="worry">
+				<div class="inner">
+					<p>人手不足や採用難にお悩みの企業様へ。<br>mintokuは採用から定着、帰国支援まで、<br>国人雇用のすべてをサポートします。<br>完全成果報酬制で、初期コストも安心。</p>
+					<img src="<?php bloginfo('template_directory'); ?>/assets/img/introduce.png" alt="導入企業数2,000社以上（2025年2月時点）" class="introduce">
+					<div class="worry-illust">
+						<picture>
+							<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/worry-illust-sp.svg" media="(max-width: 767px)">
+							<img src="<?php bloginfo('template_directory'); ?>/assets/img/worry-illust-pc.svg" alt="外国人採用こんなことで悩んでいませんか？ビザ手続きや書類管理が煩雑…採用してもすぐに辞めてしまう…言葉や文化の違いが心配…社内にサポートできる人材がいない…法令順守など、何に気をつければいいのか…">
+						</picture>
+					</div>
+				</div>
+			</section>
+			<section class="cta">
+				<div class="inner">
+					<h2>
+						<picture>
+							<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/cta-h2-sp.svg" media="(max-width: 767px)">
+							<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-h2-pc.svg" alt="“その課題すべて mintokuで解決できます。">
+						</picture>
+					</h2>
+					<p>「mintoku」は、外国人採用に特化した<br class="sp-only">トータルサポートサービスです。<br>採用前の準備から、<br class="sp-only">入社後の定着・教育・トラブル対応まで、<br>一社ごとの課題に合わせた最適な支援を提供します。</p>
+					<div class="cta-btn">
+						<div class="cta-contact">
+							<a href="https://mintoku.com/biz/contact/">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-contact.svg" alt="お問い合わせはこちら">
+							</a>
+						</div>
+						<div class="cta-tel">
+							<a href="tel:03-6738-9686">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-tel.svg" alt="お電話の方はこちらから 月〜金／10:00〜18:00 (土・日・祝日除）03-6738-9686">
+							</a>
+						</div>
+						<div class="cta-dl">
+							<a href="https://mintoku.com/biz/report_download/?category=white_paper">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-dl.svg" alt="資料ダウンロード">
+							</a>
+						</div>
+						<div class="cta-seminar">
+							<a href="https://mintoku.com/biz/seminar/">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-seminar.svg" alt="セミナー情報">
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section class="point">
+				<div class="inner">
+					<h2>
+						<picture>
+							<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/point-h2-sp.svg" media="(max-width: 767px)">
+							<img src="<?php bloginfo('template_directory'); ?>/assets/img/point-h2-pc.svg" alt="“その課題すべて mintokuで解決できます。">
+						</picture>
+					</h2>
+					<ul>
+						<li>
+							<picture>
+								<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/point01-sp.svg" media="(max-width: 767px)">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/point01-pc.svg" alt="“point01外国人雇用を、ワンストップで対応。採用から定着、帰国までトータル支援">
+							</picture>
+						</li>
+						<li>
+							<picture>
+								<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/point02-sp.svg" media="(max-width: 767px)">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/point02-pc.svg" alt="“point02特定技能ビザ対応（ドライバー・整備士など）。専門性の高い人材も紹介可能">
+							</picture>
+						</li>
+						<li>
+							<picture>
+								<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/point03-sp.svg" media="(max-width: 767px)">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/point03-pc.svg" alt="“point03勤怠管理・年末調整など労務もお任せ。面倒な事務作業を代行">
+							</picture>
+						</li>
+					</ul>
+				</div>
+			</section>
+			<section class="value">
+				<div class="inner">
+					<h2>採用“後”こそ、mintokuの真価</h2>
+					<p>外国人材が戦力となるには、<br class="sp-only">「採用して終わり」では不十分です。<br>mintokuは、就業後の定着・成長・日常の<br class="sp-only">支援までを徹底サポート。<br>長く、安心して働ける環境を、<br class="sp-only">企業と一緒に創っていきます。</p>
+					<ul class="value-point">
+						<li>
+							<picture>
+								<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/value01-sp.svg" media="(max-width: 767px)">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/value01-pc.svg" alt="“生活インフラ（住居・通信など）の契約代行">
+							</picture>
+						</li>
+						<li>
+							<picture>
+								<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/value02-sp.svg" media="(max-width: 767px)">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/value02-pc.svg" alt="“24時間・多言語対応のコールセンター">
+							</picture>
+						</li>
+						<li>
+							<picture>
+								<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/value03-sp.svg" media="(max-width: 767px)">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/value03-pc.svg" alt="“日本語教育・意識改革で定着率アップ">
+							</picture>
+						</li>
+					</ul>
+					<ul class="result">
+						<li>
+							<picture>
+								<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/result01-sp.svg" media="(max-width: 767px)">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/result01-pc.svg" alt="導入社数2,000社（2025年2月時点）">
+							</picture>
+						</li>
+						<li>
+							<picture>
+								<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/result02-sp.svg" media="(max-width: 767px)">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/result02-pc.svg" alt="事務対応時間55%削減" class="result02">
+							</picture>
+						</li>
+						<li>
+							<picture>
+								<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/result03-sp.svg" media="(max-width: 767px)">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/result03-pc.svg" alt="対応可能言語数24言語" class="result03">
+							</picture>
+						</li>
+					</ul>
+				</div>
+			</section>
+			<section class="faq">
+				<div class="inner">
+					<h2>よくある質問</h2>
+					<dl class="faq-wrap">
+						<dt>Q.鉄道運転士には免許が必要かと思いますが、試験内容はどのようなものですか？</dt>
+						<dd>A.鉄道運転士になるための動力車操縦者試験は、身体検査・適性検査・筆記試験・技能試験によって行われます。<br><a href="https://mintoku.com/biz/qa_detail/3544" class="more">詳しく見る&raquo;</a></dd>
+						
+						<dt>Q.「特定技能1号」で在留できる期間は通算5年以内とされていますが、海外にいる間も期間に含まれますか？</dt>
+						<dd>A.再入国許可により出国していた期間は「通算」期間に含まれます。ただし、例外の場合もございます。<br><a href="https://mintoku.com/biz/qa_detail/1349" class="more">詳しく見る&raquo;</a></dd>
+						
+						<dt>Q.外国人がトラックドライバーとして働くには免許が必要かと思いますが、外国の運転免許でも運転可能ですか？</dt>
+						<dd>A.免許の種類によって扱いが異なります。<br>以下にまとめましたので、ご確認ください。<br>
+							<ul>
+								<li>➀国際運転免許証を所持している場合</li>
+								<li>➁外国運転免許証（政令で定める国又は地域のみ）を所持している場合</li>
+								<li>➂外国の運転免許証から国内運転免許へ切り替える場合＜外免切替＞</li>
+								<li>➃日本で運転免許証を取得する場合<br>の４パターンがあります。</li>
+							</ul>
+							ただし、「特定技能」の場合には、➀国際運転免許証　➁外国運転免許証<br>は資格対象外になりますので、お気をつけください。<br><a href="https://mintoku.com/biz/qa_detail/3852" class="more">詳しく見る&raquo;</a></dd>
+						
+						<dt>Q.外国人労働者を雇用した場合に特別に必要な届出はありますか？</dt>
+						<dd>A.基本的には<a href="https://www.mhlw.go.jp/content/001059771.pdf" target="_blank">「外国人雇用状況届出書（様式第3号）」</a>が必要です。<br>また、外国人労働者が雇用保険被保険者である場合には、<a href="https://www.mhlw.go.jp/file/06-Seisakujouhou-11600000-Shokugyouanteikyoku/0000088973.pdf" target="_blank" class="pdflink">「雇用保険被保険者資格取得届（様式第２号・第4号）」</a>の提出を持って、「雇用状況届出書」に替えることも可能です。<br><a href="https://mintoku.com/biz/qa_detail/4068" class="more">詳しく見る&raquo;</a></dd>
+						
+						<dt>Q.外国に家族がおり、日本から仕送りをしています。令和5年1月より、年間38万円以上支払っていないと所得税の扶養控除が受けられないと聞きましたが、本当ですか？</dt>
+						<dd>A.扶養控除の対象となるか否かは、ご家族（扶養親族）の方の年齢等によります。
+							<ul>
+								<li>・令和5年1月までは、扶養親族が「16歳以上」の方であれば扶養控除の対象となっていましたが、</li>
+								<li>・令和5年1月より、16歳以上であっても「30歳以上70歳未満」の方の場合には
+									<ul>
+										<li>① 留学により国内に住所及び居所を有しなくなった者</li>
+										<li>② 障害者</li>
+										<li>③ その居住者からその年において生活費又は教育費に充てるための支払を38万円以上受けている者</li>
+									</ul>
+								</li>
+							</ul>
+							のいずれかでなければ、扶養控除の対象となる親族に該当しないこととなりました。<br>（下記の図をご参照ください）<br><a href="https://mintoku.com/biz/qa_detail/3299" class="more">詳しく見る&raquo;</a></dd>
+					</dl>
+				</div>
+			</section>
+			<section class="cta">
+				<div class="inner">
+					<h2>
+						<picture>
+							<source srcset="<?php bloginfo('template_directory'); ?>/assets/img/cta-h2-sp.svg" media="(max-width: 767px)">
+							<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-h2-pc.svg" alt="“その課題すべて mintokuで解決できます。">
+						</picture>
+					</h2>
+					<p>「mintoku」は、外国人採用に特化した<br class="sp-only">トータルサポートサービスです。<br>採用前の準備から、<br class="sp-only">入社後の定着・教育・トラブル対応まで、<br>一社ごとの課題に合わせた最適な支援を提供します。</p>
+					<div class="cta-btn">
+						<div class="cta-contact">
+							<a href="https://mintoku.com/biz/contact/">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-contact.svg" alt="お問い合わせはこちら">
+							</a>
+						</div>
+						<div class="cta-tel">
+							<a href="tel:03-6738-9686">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-tel.svg" alt="お電話の方はこちらから 月〜金／10:00〜18:00 (土・日・祝日除）03-6738-9686">
+							</a>
+						</div>
+						<div class="cta-dl">
+							<a href="https://mintoku.com/biz/report_download/?category=white_paper">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-dl.svg" alt="資料ダウンロード">
+							</a>
+						</div>
+						<div class="cta-seminar">
+							<a href="https://mintoku.com/biz/seminar/">
+								<img src="<?php bloginfo('template_directory'); ?>/assets/img/cta-seminar.svg" alt="セミナー情報">
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
+		</main>
+		<footer>
+			<div class="inner">
+				<div class="privacypolicy">
+					<a target="_blank" href="/work/term-of-use/">利用規約</a>
+					<a target="_blank" href="https://cam-global.co.jp/privacypolicy/">プライバシーポリシー</a>
+				</div>
+				<div class="copyright">
+					<p>&copy; 2025 CAMGLOBAL Inc. All Rights Reserved.</p>
+				</div>
+			</div>
+		</footer>
+		<script>
+			document.addEventListener("DOMContentLoaded", function () {
+				const dts = document.querySelectorAll(".faq-wrap dt");
+				const dds = document.querySelectorAll(".faq-wrap dd");
+
+				dts.forEach((dt, index) => {
+					dt.addEventListener("click", function () {
+						const dd = dt.nextElementSibling;
+						const isOpen = dd.classList.contains("is-open");
+
+						dts.forEach(d => d.classList.remove("is-active"));
+						dds.forEach(d => d.classList.remove("is-open"));
+
+						if (!isOpen) {
+							dt.classList.add("is-active");
+							dd.classList.add("is-open");
+						}
+					});
+				});
+			});
+		</script>
+	</body>
+</html>
